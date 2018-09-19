1c986a7a914dda15e46cd2af6232e3107aef9258
tishayaem <kostiantynkalinin@gmail.com>
2018-09-19 16:51:19 +0100

    Test and build Wallet component

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux'
 export class Wallet extends React.Component {
     render() {
         return(
-            <h1>Wallet balance: {this.props.balance}</h1>
+            <h1 className='balance'>Wallet balance: {this.props.balance}</h1>
         )
     }
 }
 
-export default connect( state => { balance: state} , null)(Wallet);
+export default connect( state => { return {balance: state} }, null)(Wallet);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import App from "./components/App";
 import { createStore } from "redux";
 import rootReducer from "./reducers/balance";
-import { Provider } from "./components/App";
+import { Provider } from "react-redux";
 
 const store = createStore(rootReducer);
 
